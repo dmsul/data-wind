2b753b50224541a17f54aaaa221baf9b47c0f078
Jieyi Lu <jl2210@georgetown.edu>
2018-11-14 15:37:34 -0500

    use netcdf to read nc file

--- winddata/clean/raw.py
+++ winddata/clean/raw.py
@@ -1,17 +1,18 @@
-import pandas as pd
 import xarray as xr
 
 from winddata.util.env import data_path
+
 
 def load_winddata(direction, year):
     filepath = data_path(f'{direction}wnd.sig995.{year}.nc')
 
     netcdf = xr.open_dataset(filepath)
-    df = pd.DataFrame(netcdf['nbnds'].data)
-    df.index = netcdf['lat'].data
-    df.columns = netcdf['lon'].data
+    df = netcdf.to_dataframe()
+    df = df.reset_index()
 
     return df
 
+
 if __name__ == '__main__':
-    print(load_winddata("v", 2015))
+    df = load_winddata("v", 2015)
+    print(df.head())
